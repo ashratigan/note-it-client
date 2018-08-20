bb2ac90682aed5ab3dd98cd28015f876a65bd538
Ashley <ashley.ratigan@gmail.com>
2018-08-20 14:12:40 -0400

    Made notes draggable

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import Draggable from 'react-draggable';
 import '../styles/Note.css'
 
 const Note = props => {
 
+  
+
   return (
-    <div className="Note-div">
+    
+    <Draggable
+      handle=".Note-div"
+      defaultPosition={{x: 0, y: 0}}
+      position={null}
+      grid={[25, 25]}
+      onStart={this.handleStart}
+      onDrag={this.handleDrag}
+      onStop={this.handleStop}>
+      <div className="Note-div">
         <p>{props.title}</p>
         <p>{props.content}</p>
         {/* <div>{props.notesAPI}</div> */}
     </div>
+    </Draggable>
+
   )
 }
 
